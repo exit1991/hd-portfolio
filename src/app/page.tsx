0fd0de63fd72cb5606b60df319cf3537ea0e22a7
hdeg <76611318+exit1991@users.noreply.github.com>
2024-02-10 21:55:43 +0900

    ページ用資材追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,220 @@
+"use client";
+import * as THREE from "three";
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import { useEffect } from "react";
+
+import "ress";
+import "./style.scss";
 
 export default function Home() {
+  useEffect(() => {
+    main();
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <canvas id="myCanvas" />
+      <main>
+        <div className="wrap">
+          <nav className="nav-list">
+            <ul>
+              <li className="nav-list-text">
+                <a href="/">about</a>
+              </li>
+              <li className="nav-list-text">
+                <a href="/">works</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="logo-wrap">
+            <div className="logo">
+              <svg
+                width={131}
+                height={130}
+                viewBox="0 0 131 130"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="65.1844" cy={65} r={65} fill="#EDEDED" />
+                <rect
+                  x="34.7156"
+                  y="58.9062"
+                  width="48.75"
+                  height="48.75"
+                  fill="#84CFE9"
+                />
+                <rect
+                  x="59.0906"
+                  y="34.5312"
+                  width="48.75"
+                  height="48.75"
+                  fill="#FFE153"
+                />
+                <rect
+                  x="22.5281"
+                  y="22.3438"
+                  width="48.75"
+                  height="48.75"
+                  fill="#EE806A"
+                />
+              </svg>
+              <h1>hdeg</h1>
+            </div>
+          </div>
+          <section className="about">
+            <h1 className="about__title">about</h1>
+            <div className="about__desc">
+              <p>h deg</p>
+            </div>
+          </section>
+          <section className="works">
+            <h1 className="works__title">works</h1>
+            <div className="works__desc">
+              <p>h deg</p>
+            </div>
+          </section>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="scroll-dummy">
+          <p>page end</p>
+        </div>
+      </main>
+    </>
   );
+}
+
+const START_Z_POSITION: number = 1500;
+
+function main() {
+  // サイズを指定
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    canvas: document.querySelector("#myCanvas") ?? undefined,
+  });
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  // カメラの位置
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = START_Z_POSITION;
+
+  function scrollCameraMove() {
+    camera.lookAt(0, 0, 0);
+    const scrollY = window.scrollY;
+
+    const changeValue = START_Z_POSITION + 400;
+    if (scrollY <= changeValue) {
+      camera.position.x = 0;
+      camera.position.y = 0;
+      camera.position.z = START_Z_POSITION - scrollY * 0.8;
+
+      camera.rotation.x = 0;
+      camera.rotation.y = 0;
+      camera.rotation.z = 0;
+
+      const halfPositonZ = START_Z_POSITION / 2;
+      if (scrollY >= halfPositonZ) {
+        camera.rotation.z = Math.PI * ((scrollY - halfPositonZ) * 0.0005);
+      }
+    } else {
+      const halfPositonZ = START_Z_POSITION / 2;
+
+      // // ラジアンに変換する
+      // const radian = ((scrollY - START_Z_POSITION) * 0.01) * Math.PI / 180;
+      // // 角度に応じてカメラの位置を設定
+      // const xValue = START_Z_POSITION * 0.17;
+      // const zValue = START_Z_POSITION * 0.067;
+      // const positionX = xValue * Math.sin(radian);
+      // const positionZ = (zValue - (zValue * Math.cos(radian))) * -1;
+      // camera.position.x = positionX;
+      // camera.position.z = START_Z_POSITION - (START_Z_POSITION * 0.8) + positionZ;
+
+      camera.rotation.x = 0;
+      camera.rotation.y = 0;
+      // camera.rotation.z = Math.PI * ((START_Z_POSITION - halfPositonZ) * 0.0005);
+      camera.rotation.z = Math.PI * ((changeValue - halfPositonZ) * 0.0005);
+
+      // ラジアンに変換する
+      // const radian = ((scrollY - 1200) * 0.01) * Math.PI / 180;
+      const radian = ((scrollY - 1200) * 0.02 * Math.PI) / 180;
+      // 角度に応じてカメラの位置を設定
+      const positionX = 200 * Math.sin(radian);
+      const positionZ = (200 - 200 * Math.cos(radian)) * -1;
+      camera.position.x = positionX;
+      camera.position.z = changeValue - changeValue * 0.8 + positionZ;
+      // camera.position.z = positionZ;
+    }
+  }
+
+  scrollCameraMove();
+  window.addEventListener("scroll", () => {
+    scrollCameraMove();
+  });
+
+  createParticles(0xee806a);
+  createParticles(0xffe153);
+  createParticles(0x84cfe9);
+  tick();
+
+  //パーティクルの作成
+  function createParticles(materialColor: THREE.ColorRepresentation) {
+    // 頂点情報を格納する配列
+    const vertices = [];
+
+    // 配置する個数
+    // const LENGTH = 1000;
+    // const LENGTH = 800;
+    const LENGTH = 300;
+    const range = 500;
+    for (let i = 0; i < LENGTH; i++) {
+      // 頂点の初期座標
+      vertices.push(
+        Math.random() * range - range / 2,
+        Math.random() * range - range / 2,
+        Math.random() * range - range / 2
+      );
+    }
+
+    // 形状データを作成
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(vertices, 3)
+    );
+
+    // マテリアルを作成
+    const material = new THREE.PointsMaterial({
+      // size: 3, // 一つ一つのサイズ
+      // size: 4.5, // 一つ一つのサイズ
+      // size: 10, // 一つ一つのサイズ
+      size: 8, // 一つ一つのサイズ
+      color: materialColor, // 色
+    });
+
+    // 物体を作成
+    scene.add(new THREE.Points(geometry, material));
+  }
+
+  // 毎フレーム時に実行されるループイベント
+  function tick() {
+    // cloud.rotation.y += 0.0001;
+    renderer.render(scene, camera); // レンダリング
+    requestAnimationFrame(tick);
+  }
 }
