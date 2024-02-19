996dcbe6dd19e439cfa5ecc5520355d3c203ede5
hdeg <76611318+exit1991@users.noreply.github.com>
2024-02-19 18:45:41 +0900

    Safariでロード画面が消えない事象を修正

--- src/components/elements/SplashScreen.tsx
+++ src/components/elements/SplashScreen.tsx
@@ -8,13 +8,19 @@ type Props = {
 
 export default function Home({ visibleTime }: Props) {
   const [isLoading, setLoading] = useState(true);
+  const [isDisplay, setDisplay] = useState(true);
   useEffect(() => {
     setTimeout(() => setLoading(false), visibleTime);
+    setTimeout(() => setDisplay(false), visibleTime + 1000);
   }, [visibleTime]);
 
   return (
     <>
-      <div className={`p-splash-screen ${isLoading ? '' : 'is-hide'}`}>
+      <div
+        className={`p-splash-screen ${isLoading ? '' : 'is-hide'} ${
+          isDisplay ? '' : 'is-display-none'
+        }`}
+      >
         <div className="p-splash-screen__logo">
           <SplashLogo />
         </div>
